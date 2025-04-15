262e9e3dbe668836d37dc2913ff54b064690a1e5
Alixandria007 <juninhokrauts778@gmail.com>
2025-04-15 19:48:59 -0300

    View de user

--- backend/apps/user/views.py
+++ backend/apps/user/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+class UserObjectView(APIView):
+    def get(self, request):
+        ...
+
+class UserCreateView(APIView):
+    def post(self, request):
+        ...
+
+class UserUpdateView(APIView):
+    def patch(self, request):
+        ...
+
+class UserDeleteView(APIView):
+    def patch(self, request):
+        ...
+
+class GoalsObjectView(APIView):
+    def get(self, request):
+        ...
+
+class GoalsUpdateView(APIView):
+    def patch(self, request):
+        ...
