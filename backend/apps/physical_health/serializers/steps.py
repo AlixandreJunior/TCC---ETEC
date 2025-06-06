ed9e1ac6323896c53b71b39cd7c26e11b1bccfc8
AlixandreJunior <alixandreonetojunior@gmail.com>
2025-06-06 11:00:16 -0300

    Dividi os serializers de 'Physical_Health'

---
+++ backend/apps/physical_health/serializers/steps.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from apps.physical_health.models.steps import StepsLog
+
+class StepsLogSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = StepsLog
+        fields = ['id', 'user', 'steps', 'goal_achieved', 'date']
